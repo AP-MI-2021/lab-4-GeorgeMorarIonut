073cb53fbfcad7b11bfef132515024751cef0785
unknown <morargeorge_02@yahoo.com>
2021-10-20 20:13:16 +0300

    complet

--- main.py
+++ main.py
@@ -103,14 +103,93 @@ def testNumereSuperPrime():
     assert numereSuperPrime([23, 239, 12, 90, 373, 20]) == [23, 239, 373]
     assert numereSuperPrime([42, 87, 94, 88, 32]) == []
 
-def main():
+def celMaiMareDivCom(nr1 ,nr2):
+    """
+    Determina cel mai mare divizor comun dintre doua numere.
+    :param nr1: numar intreg
+    :param nr2: numar intreg
+    :return: cel mai mare divizor comun dintre doua numere
+    """
+    if nr1 == 0:
+        if nr2 == 0:
+            return -9999
+        else:
+            return nr2
+    else:
+        while nr2 != 0:
+            r = nr1 % nr2
+            nr1 = nr2
+            nr2 = r
+        return nr1
 
-    lst = []
+def testCelMaiMareDivCom():
+    assert celMaiMareDivCom(12, 144) == 12
+    assert celMaiMareDivCom(6, 12) == 6
+    assert celMaiMareDivCom(24, 16) == 8
+
+
+def cmmdcNumerePozitive(lst):
+    """
+    Determina cmmdc-ul numerelor pozitive dintr-o lista.
+    :param lst: o lista de numere
+    :return: cmmdc-ul numerelor pozitive dintr-o lista
+    """
+    cmmdc = 0
+    for i in lst:
+        if i > 0:
+            cmmdc = celMaiMareDivCom(cmmdc, i)
+    return cmmdc
+
+def testCmmdcNumerePozitive():
+    assert cmmdcNumerePozitive([-2, 24, 4, 13, -16, 12]) == 1
+    assert cmmdcNumerePozitive([6, 12, -3, 4, 3]) == 1
+    assert cmmdcNumerePozitive([-76, 12, 24, -13, 144]) == 12
+
+
+def inversSiCmmdc(lst):
+    """
+    Afișarea listei obținute din lista inițială în care numerele pozitive și nenule au fost înlocuite cu
+    CMMDC-ul lor și numerele negative au cifrele în ordine inversă.
+    :param lst: o lista de numere
+    :return:lista obținute din lista inițială în care numerele pozitive și nenule au fost înlocuite cu
+    CMMDC-ul lor și numerele negative au cifrele în ordine inversă
+    """
+    rezultat = []
+    for i in lst:
+        if i > 0:
+            rezultat.append(cmmdcNumerePozitive(lst))
+        elif i <= 0:
+            iStr = str(i)
+            x = iStr.split("-")[1]
+            x = x[::-1]
+            x = int(x)
+            x = 0 - x
+            rezultat.append(x)
+    return rezultat
+
+
+def testInversSiCmmdc():
+    assert inversSiCmmdc([-76, 12, 24, -13, 144]) == [-67, 12, 12, -31, 12]
+    assert inversSiCmmdc([12, 4, -87, -34, 16]) == [4, 4, -78, -43, 4]
+    assert inversSiCmmdc([]) == []
+
+
+def testAll():
     testAfisareNumereNegativeNenule()
     testSmallestLastDigit()
     testIsPrime()
     testIsSuperprime()
     testNumereSuperPrime()
+    testCelMaiMareDivCom()
+    testCmmdcNumerePozitive()
+    testInversSiCmmdc()
+
+
+def main():
+
+    lst = [12, -2, 24]
+    testAll()
+
 
     while True:
         print("1. Citire")
@@ -118,7 +197,9 @@ def main():
         print("2. Afiseaza numerele negative din lista citita.")
         print("3. Afiseaza cel mai mic număr care are ultima cifră egală cu o cifră citită de la tastatură")
         print("4. Afișarea tuturor numerelor din listă care sunt superprime.")
+        print("5. Ex 5")
         print("x. Iesire")
+
 
         optiune = input("Dati optiune: ")
         if optiune == "1":
@@ -132,8 +213,12 @@ def main():
             print(smallestLastDigit(lst, k))
         elif optiune == "4":
             print(numereSuperPrime(lst))
+        elif optiune == "5":
+            print(inversSiCmmdc(lst))
         elif optiune == "x":
-            break
+           break
+        else:
+           print("Optiune gresita! Selectati alta optiune!")
 
 
 
